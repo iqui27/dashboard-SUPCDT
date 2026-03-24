c09a4e7dec7bc741d03d2b2bc111547de733d761
Henrique Rocha <henriquevrocha@me.com>
2026-03-24 19:47:46 -0300

    fix: use correct collection name (custom_projects)

--- server/services/projetos.ts
+++ server/services/projetos.ts
@@ -2,7 +2,7 @@ import { ObjectId } from 'mongodb';
 import { getDatabase } from '../db/client.js';
 import { DBProjeto, DBProjetoMonitoramento, ProjetoApi, ProjetoInput } from '../types/projeto.js';
 
-const COLLECTION_NAME = 'projetos_supcdt';
+const COLLECTION_NAME = 'custom_projects';
 
 type ResolvedMonitoramentoOperacional = {
   statusOperacional: string;
