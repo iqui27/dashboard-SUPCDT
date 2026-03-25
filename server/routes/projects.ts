84a6ec86ce4a51220b7a46f383148333716ebe86
Henrique Rocha <henriquevrocha@me.com>
2026-03-25 14:07:47 -0300

    fix: revert /api/projects to use custom_projects for other dashboard

--- server/routes/projects.ts
+++ server/routes/projects.ts
@@ -6,7 +6,7 @@ import { getDatabase } from '../db/client.js';
 import { extractProjectFromPdf } from '../services/geminiImport.js';
 import { requireAuth } from '../middleware/auth.js';
 
-const COLLECTION_NAME = 'projetos_supcdt';
+const COLLECTION_NAME = 'custom_projects';
 
 interface CustomProjectDoc {
   _id?: ObjectId | string;
